984062b3cad39f3cf920fb393497908208911a18
masozow <14838605+masozow@users.noreply.github.com>
2025-05-05 09:29:59 -0600

    crating two models

---
+++ api/models/Stock.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace api.models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty; 
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDividend { get; set; }
+        public string Industry { get; set; } = string.Empty;    
+        public long MarketCap { get; set; }
+        public List<Comment> Sector { get; set; } = new List<Comment>();
+    }
+}
