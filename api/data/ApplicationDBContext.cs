22337464e710bbe0ecda4d51e326c7b4ace94706
masozow <14838605+masozow@users.noreply.github.com>
2025-05-06 16:55:20 -0600

    adding users and a register endpoint to create a new user

--- api/data/ApplicationDBContext.cs
+++ api/data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,5 +18,16 @@ namespace api.data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole {Id="1",Name = "Admin", NormalizedName = "ADMIN"},
+                new IdentityRole {Id="2",Name = "User", NormalizedName = "USER"}
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
