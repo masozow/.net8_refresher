40e6b6f7117c8c94bdc598f6d0efc77c3694105b
masozow <14838605+masozow@users.noreply.github.com>
2025-05-06 16:20:11 -0600

    creating JWT identity

--- api/data/ApplicationDBContext.cs
+++ api/data/ApplicationDBContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace api.data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContextOptions)
             : base(dbContextOptions)
