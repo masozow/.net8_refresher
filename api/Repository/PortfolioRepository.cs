df07eadc1cfc4d33228740953d32360fbc82ebf3
masozow <14838605+masozow@users.noreply.github.com>
2025-05-07 11:47:22 -0600

    adding the portfolio config to read from it and adding manually one to test it out

---
+++ api/Repository/PortfolioRepository.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.data;
+using api.interfaces;
+using api.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public PortfolioRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return await _context.Portfolios.Where(u => u.AppUserId == user.Id).Select(stock => new Stock
+            {
+                Id = stock.StockId,
+                Symbol = stock.Stock.Symbol,
+                CompanyName = stock.Stock.CompanyName,
+                Price = stock.Stock.Price,
+                LastDividend = stock.Stock.LastDividend,
+                Industry = stock.Stock.Industry,
+                MarketCap = stock.Stock.MarketCap
+            }).ToListAsync();
+        }
+    }
+}
