1f335b457b59df23b1b0e787ca327b4548c0a690
masozow <14838605+masozow@users.noreply.github.com>
2025-05-16 16:31:18 -0600

    adding an axios call to an API

--- frontend/src/components/search-form.tsx
+++ frontend/src/components/search-form.tsx
@@ -2,8 +2,17 @@ import { Search } from "lucide-react"
 
 import { Label } from "@/components/ui/label"
 import { SidebarInput } from "@/components/ui/sidebar"
+import { useState, type JSX } from "react"
 
-export function SearchForm({ ...props }: React.ComponentProps<"form">) {
+
+interface SearchFormProps extends React.ComponentProps<"form"> {
+  searchTerm: string
+}
+export const SearchForm:React.FC<SearchFormProps> = ({ ...props }: SearchFormProps):JSX.Element =>{
+  const [search, setSearch] = useState<string>("");
+  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  }
   return (
     <form {...props}>
       <div className="relative">
@@ -14,9 +23,14 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
           id="search"
           placeholder="Type to search..."
           className="h-8 pl-7"
+          value={search}
+          onChange={onSearchChange} 
         />
         <Search className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
+        
       </div>
+      <p>{search}</p>
     </form>
   )
 }
+
