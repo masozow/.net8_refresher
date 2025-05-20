409bc79e4641871184b2684e7d2670a23f4cec36
masozow <14838605+masozow@users.noreply.github.com>
2025-05-20 14:37:03 -0600

    adding an api call and asssigning that function to company.tsx page

--- frontend/src/API/company.d.ts
+++ frontend/src/API/company.d.ts
@@ -7,41 +7,42 @@ export interface CompanySearch {
 }
 
 export interface CompanyProfile {
-  symbol: string;
-  price: number;
-  beta: number;
-  volAvg: number;
-  mktCap: number;
-  lastDiv: number;
-  range: string;
-  changes: number;
-  companyName: string;
-  currency: string;
-  cik: string;
-  isin: string;
-  exchange: string;
-  exchangeShortName: string;
-  industry: string;
-  website: string;
-  description: string;
-  ceo: string;
-  sector: string;
-  counter: string;
-  fullTimeEmployees: string;
-  phone: string;
-  address: string;
-  city: string;
-  state: string;
-  zip: string;
-  dcfDiff: number;
-  dcf: number;
-  image: string;
-  ipoDate: string;
-  defaultImage: boolean;
-  isEtf: boolean;
-  isActivelyTrading: boolean;
-  isAdr: boolean;
-  isFund: boolean;
+  symbol: string
+  price: number
+  marketCap: number
+  beta: number
+  lastDividend: number
+  range: string
+  change: number
+  changePercentage: number
+  volume: number
+  averageVolume: number
+  companyName: string
+  currency: string
+  cik: string
+  isin: string
+  cusip: string
+  exchangeFullName: string
+  exchange: string
+  industry: string
+  website: string
+  description: string
+  ceo: string
+  sector: string
+  country: string
+  fullTimeEmployees: string
+  phone: string
+  address: string
+  city: string
+  state: string
+  zip: string
+  image: string
+  ipoDate: string
+  defaultImage: boolean
+  isEtf: boolean
+  isActivelyTrading: boolean
+  isAdr: boolean
+  isFund: boolean
 }
 
 export interface CompanyKeyRatios {
--- frontend/src/pages/company/Company.tsx
+++ frontend/src/pages/company/Company.tsx
@@ -1,10 +1,43 @@
+import { getCompanyProfile } from "@/API/api";
+import type { CompanyProfile } from "@/API/company";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router"
+
 interface Props  {
     
 }
 
 const Company = (props: Props) => {
+  const {ticker} = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+  useEffect(() => {
+    const getCompany = async () => {
+      if (ticker) {
+        const response = await getCompanyProfile(ticker);
+        console.log('Response: ', response);
+          if (Array.isArray(response) && response.length > 0) {
+            setCompany(response[0]);
+          } else if (typeof response === "string") {
+            console.error("Error fetching company profile:", response);
+          }
+      }
+    }
+    getCompany();
+  },[ticker]);
+
   return (
-    <div>Company</div>
+    <>
+    {
+    company ?
+    ( 
+      <div>
+        <h1>{company.companyName}</h1>
+      </div>
+    ):
+      <h1>Company not found</h1>
+
+    }
+    </>
   )
 }
 
