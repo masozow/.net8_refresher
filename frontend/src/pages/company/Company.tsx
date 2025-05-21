a4fb54cfbb1d2eeb9a609e0536fcfb27c42b7012
masozow <14838605+masozow@users.noreply.github.com>
2025-05-21 08:24:10 -0600

    adding shadcn tabs component to the compnay page

--- frontend/src/pages/company/Company.tsx
+++ frontend/src/pages/company/Company.tsx
@@ -1,48 +1,60 @@
 import { getCompanyProfile } from "@/API/api";
 import type { CompanyProfile } from "@/API/company";
 import Tile from "@/components/Tile/Tile";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useEffect, useState } from "react";
-import { Outlet, useParams } from "react-router"
+import { Outlet, useParams } from "react-router";
 import { NavLink } from "react-router-dom";
 
-
-
 const Company = () => {
-  const {ticker} = useParams();
+  const { ticker } = useParams();
   const [company, setCompany] = useState<CompanyProfile>();
   useEffect(() => {
     const getCompany = async () => {
       if (ticker) {
         const response = await getCompanyProfile(ticker);
-        console.log('Response: ', response);
-          if (Array.isArray(response) && response.length > 0) {
-            setCompany(response[0]);
-          } else if (typeof response === "string") {
-            console.error("Error fetching company profile:", response);
-          }
+        console.log("Response: ", response);
+        if (Array.isArray(response) && response.length > 0) {
+          setCompany(response[0]);
+        } else if (typeof response === "string") {
+          console.error("Error fetching company profile:", response);
+        }
       }
-    }
+    };
     getCompany();
-  },[ticker]);
+  }, [ticker]);
 
   return (
     <div className="w-full px-10">
-      {
-      company ?
-      ( 
+      {company ? (
         <div>
-          <Tile title={company.companyName} subtitle={company.symbol}/>
-          <div className="flex w-full bg-gray-200 rounded">
-            <NavLink className={({isActive}) => `p-2 ${isActive ? "bg-gray-400" : ""}`} to={`company-profile`}>Profile</NavLink>
-            <NavLink className={({isActive}) => `p-2 ${isActive ? "bg-gray-400" : ""}`} to={`income-statement`}>Income statement</NavLink>
-          </div>
-          <Outlet/>
+          <Tile title={company.companyName} subtitle={company.symbol} />
+          <Tabs defaultValue="profile" className="w-[400px]">
+            <TabsList>
+              <TabsTrigger value="company-profile">
+                <NavLink to="company-profile">Company Profile</NavLink>
+              </TabsTrigger>
+              <TabsTrigger value="income-statement">
+                <NavLink to="income-statement">Income statement</NavLink>
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="company-profile"
+              defaultChecked
+              className="px-3"
+            >
+              <Outlet />
+            </TabsContent>
+            <TabsContent value="income-statement" className="px-3">
+              <Outlet />
+            </TabsContent>
+          </Tabs>
         </div>
-      ):
+      ) : (
         <h1>Company not found</h1>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
