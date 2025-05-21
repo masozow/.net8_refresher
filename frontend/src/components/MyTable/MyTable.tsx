d1cd11fafe6586c655bef7c615451aa4cde05832
masozow <14838605+masozow@users.noreply.github.com>
2025-05-21 09:27:06 -0600

    creating functions to populate MyTable headers and body independently

--- frontend/src/components/MyTable/MyTable.tsx
+++ frontend/src/components/MyTable/MyTable.tsx
@@ -1,7 +1,52 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { testIncomeStatementData } from "./testData";
+
+const data = testIncomeStatementData;
+type Company = (typeof data)[0];
+
+const configs = [
+  {
+    label: "Year",
+    render: (company: Company) => company.acceptedDate,
+  },
+  {
+    label: "Cost of Revenue",
+    render: (company: Company) => company.costOfRevenue,
+  },
+];
+
 interface Props {}
 
 const MyTable = (props: Props) => {
-  return <div>MyTable</div>;
+  const renderedRows = data.map((company) => (
+    <TableRow key={company.cik}>
+      {configs.map((config) => (
+        <TableCell key={config.label}>{config.render(company)}</TableCell>
+      ))}
+    </TableRow>
+  ));
+  const renderedHeader = configs.map((config) => (
+    <TableHead key={config.label} className="font-bold tracking-widest ">
+      {config.label}
+    </TableHead>
+  ));
+  return (
+    <Table className="mt-4">
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>{renderedHeader}</TableRow>
+      </TableHeader>
+      <TableBody>{renderedRows}</TableBody>
+    </Table>
+  );
 };
 
 export default MyTable;
